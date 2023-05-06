b5356d789e1c143c9e949faae4a4c136f162aa14
MICHIANTO <132529288+MICHIANTO@users.noreply.github.com>
2023-05-05 19:43:18 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination"
+        clickable:true,
+    }
+    navigation: {
+        nextEl:".swiper-button-next"
+        pervEl:".swiper-button-prev"
+    }
+}) 
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView:4,
+    spaceBatween: 30,
+    loop:true,
+    loopFillGroupWidthBlank:true,    
+    navigation: {
+        nextEl:".swiper-button-next"
+        pervEl:".swiper-button-prev"
+    }
+    breakpoints : {
+        0:{
+            slidesPerView:1
+        },
+        520:{
+            slidesPerView:2
+        }
+        950:{
+            slidesPerView:3
+        }
+    }
+}) 
